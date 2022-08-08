642831b45d5b41351e91009b6c2123766dad84a4
Elienock <lubayaelienock@gmail.com>
2022-08-08 10:18:32 +0200

    a few changes to the key

--- lib/widgets/wordle_key.dart
+++ lib/widgets/wordle_key.dart
@@ -7,21 +7,27 @@ class WordleKey extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget keyCap ;
+    double width = 60;
     if(letter =="_"){
-      keyCap =  Icon(Icons.keyboard_return);
+      keyCap =  Icon(Icons.keyboard_return, size: 20);
     }else if(letter == "<"){
-      keyCap = Icon(Icons.backspace_outlined);
+      keyCap = Icon(Icons.backspace_outlined, size: 20,);
     }else{
-      keyCap = Text(letter);
+      width = 40;
+      keyCap = Text(letter, style: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold
+      ),);
     }
 
     return Container(
-      width: 40,
+      width: width,
       height: 60,
       alignment: Alignment.center,
       //padding:EdgeInsets.all(10),
-      margin: EdgeInsets.all(2),
+      margin: EdgeInsets.all(4),
       decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(4)),
        color: Color.fromARGB(44, 44, 44, 44)
       ),
       child: keyCap,
