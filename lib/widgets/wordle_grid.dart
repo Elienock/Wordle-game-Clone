4094e4b1fdb2e81bb386e3c5684f107be0b6c039
Elienock <lubayaelienock@gmail.com>
2022-08-08 09:03:12 +0200

    cheecking the alignment

---
+++ lib/widgets/wordle_grid.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class WordleGrid extends StatelessWidget {
+  const WordleGrid({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("grid"),
+    );
+  }
+}
---
+++ lib/widgets/wordle_title_bar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WordleTitleBar extends StatelessWidget {
+  const WordleTitleBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("title bar"),
+    );
+  }
+}
