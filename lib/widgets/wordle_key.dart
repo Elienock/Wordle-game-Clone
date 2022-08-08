d55a5dfed3bca67c176e41e8fe8d040176103f66
Elienock <lubayaelienock@gmail.com>
2022-08-08 18:41:47 +0200

    handle wordsize and attempts

--- lib/widgets/wordle_key.dart
+++ lib/widgets/wordle_key.dart
@@ -7,25 +7,25 @@ class WordleKey extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget keyCap ;
-    double width = 60;
+    double width = 50;
     if(letter =="_"){
-      keyCap =  Icon(Icons.keyboard_return, size: 20);
+      keyCap =  Icon(Icons.keyboard_return, size: 18);
     }else if(letter == "<"){
-      keyCap = Icon(Icons.backspace_outlined, size: 20,);
+      keyCap = Icon(Icons.backspace_outlined, size: 18,);
     }else{
-      width = 40;
+      width = 35;
       keyCap = Text(letter, style: TextStyle(
-        fontSize: 20,
+        fontSize: 18,
         fontWeight: FontWeight.bold
       ),);
     }
 
     return Container(
       width: width,
-      height: 60,
+      height: 55,
       alignment: Alignment.center,
       //padding:EdgeInsets.all(10),
-      margin: EdgeInsets.all(4),
+      margin: EdgeInsets.all(3),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.all(Radius.circular(4)),
        color: Color.fromARGB(44, 44, 44, 44)
